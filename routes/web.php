6e1b7f260b6d384053d8cd64e59372a154d5fa16
Belen07-dont <b.arias17900@gmail.com>
2025-09-29 14:12:05 -0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\CrearUsuario;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/productos', [ProductController::class, 'index'])->name('productos.index');
+
+Route::post(uri: '/guardar', action: [CrearUsuario::class, 'guardar']);
+Route::post(uri: '/logout', action: [CrearUsuario::class, 'logout']);
+Route::post(uri: '/login', action: [CrearUsuario::class, 'login']);
+
+//posts para productos
+Route::post('/create-product', [PostController::class, 'createprod']);
+
+
+// Index
+Route::get('/', function () {
+    return view('index');
+});
+
+// Login 
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+
+Route::get('/signin', function () {
+    return view('sign');
+});
+
+
+
+Route::get('/carrito', function () {
+    return view('carrito');
+});
+

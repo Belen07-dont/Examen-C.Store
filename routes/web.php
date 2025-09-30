38c5014d4c35157ffcb41aca3f2c25e951d60387
Belen07-dont <b.arias17900@gmail.com>
2025-09-29 20:51:16 -0600

    new file:   app/Http/Controllers/Auth/ConfirmPasswordController.php new file:   app/Http/Controllers/Auth/ForgotPasswordController.php 	new file:   app/Http/Controllers/Auth/LoginController.php 	new file:   app/Http/Controllers/Auth/RegisterController.php 	new file:   app/Http/Controllers/Auth/ResetPasswordController.php 	new file:   app/Http/Controllers/Auth/VerificationController.php 	deleted:    app/Http/Controllers/Cart.php 	new file:   app/Http/Controllers/CartController.php 	modified:   app/Http/Controllers/Controller.php 	new file:   app/Http/Controllers/HomeController.php 	modified:   app/Http/Controllers/ProductController.php 	new file:   app/Models/Cart.php 	modified:   composer.json 	modified:   composer.lock 	modified:   package.json modified:   resources/js/bootstrap.js new file:   resources/sass/_variables.scss 	new file:   resources/sass/app.scss     new file:   resources/views/auth/login.blade.php 	new file:   resources/views/auth/passwords/confirm.blade.php 	new file:   resources/views/auth/passwords/email.blade.php 	new file:   resources/views/auth/passwords/reset.blade.php 	new file:   resources/views/auth/register.blade.php 	new file:   resources/views/auth/verify.blade.php 	modified:   resources/views/carrito.blade.php 	new file:   resources/views/home.blade.php 	new file:   resources/views/layouts/app.blade.php 	modified:   resources/views/productos.blade.php 	modified:   routes/web.php 	modified:   vite.config.js

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,25 @@
 <?php
 
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\CrearUsuario;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProductController;
-use App\Models\Cart;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrearUsuario;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('index');
 });
 
 Route::get('/productos', [ProductController::class, 'index'])->name('productos.index');
-Route::post(uri: '/cart', action: [CartController::class, 'getCartWithProducts']);
 
 Route::post(uri: '/guardar', action: [CrearUsuario::class, 'guardar']);
-
 Route::post(uri: '/logout', action: [CrearUsuario::class, 'logout']);
-
 Route::post(uri: '/login', action: [CrearUsuario::class, 'login']);
 
-//posts para productos
-Route::post('/create-product', [PostController::class, 'createprod']);
 
+//lista para productos
+Route::post('/create-product', [PostController::class, 'createprod']);
 
 // Index
 Route::get('/', function () {
@@ -44,9 +39,13 @@ Route::get('/signin', function () {
     return view('sign');
 });
 
-
+//Direcciones de Carrito
+Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart');
+Route::get('/cart', [CartController::class, 'index'])->name('carrito.index');
+    
 
 Route::get('/carrito', function () {
     return view('carrito');
 });
+
 
